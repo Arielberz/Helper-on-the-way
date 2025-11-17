0abb81135f6d726bf0bb6974aa6efa5310ceeada
Arielberz <aaariel.m.b@icloud.com>
2025-11-17 09:58:53 +0200

    Add routing and authentication pages with header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/Header'
+import Home from './pages/home/home'
+import Login from './pages/login/Login'
+import Register from './pages/register/register'
 
 function App() {
   
 
   return (
     <>
-    <h1>hello world</h1>
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
