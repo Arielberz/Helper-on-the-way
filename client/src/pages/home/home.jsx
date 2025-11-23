114e89fbd0aeb3c91ba9c76185a27fad121de911
noam <noamlu88@gmail.com>
2025-11-23 11:41:33 +0200

    Add initial UI components for profile, sidebar, help, helper,

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Header_in from "../../components/header/Header.jsx";
 import MapLive from "../../components/MapLive/MapLive.jsx";
 
 export default function Home() {
   return (
     <>
-      <Header_in />
       <MapLive />
     </>
   );
