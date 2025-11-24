d095a5945f6d2bfbd786789bd9a019a0fda51586
Arielberz <aaariel.m.b@icloud.com>
2025-11-24 12:24:16 +0200

    add getMe endpoint profile

--- Server/Api/routers/userRouter.js
+++ Server/Api/routers/userRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const{ register, login, getLocationFromIP } = require('../Controllers/userController');
+const{ register, login, getLocationFromIP, getMe } = require('../Controllers/userController');
 const { getRatingsByHelper } = require('../Controllers/ratingController');
+const authMiddleware = require('../authMiddleware');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', authMiddleware, getMe);
 router.get('/location/ip', getLocationFromIP);
 
 // Get all ratings for a specific helper
